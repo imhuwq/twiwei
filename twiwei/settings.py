97c465ffffb9536354157f64f0445ce1b343d9ba
huwq <imhuwq@gmail.com>
2016-07-10 00:04:41 +0800

    improve:[app] 运行 app 时自动生成随机的 secret key

--- twiwei/settings.py
+++ twiwei/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import random
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,7 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$ozh&ey+&33^7mb*hs8j-^p%^ol#f=76zdi(w@5y!=uagrh-r$'
+# SECRET_KEY = '$ozh&ey+&33^7mb*hs8j-^p%^ol#f=76zdi(w@5y!=uagrh-r$'
+CHARS = 'abcdefghijklmnopqrstuvwxyz' \
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ' \
+        '0123456789~!@#$%^&*(-_=+)'
+SECRET_KEY = ''.join(random.SystemRandom().choice(CHARS) for _ in range(50))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
