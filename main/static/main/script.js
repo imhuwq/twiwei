d28e246fe76ce96dd15c946fa6d375e61814440a
huwq <imhuwq@gmail.com>
2016-07-14 01:09:08 +0800

    improve:[main-timeline] 支持通过首页底部的“加载更多”来获取更早的消息

--- main/static/main/script.js
+++ main/static/main/script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     var load_more = $("#id-load-more");
     load_more.on('click', function () {
         $.get($SCRIPT_ROOT + 'load_more/', {}, function (data) {
-            alert(data.content);
             if (data.status == 200) {
                 var statuses = data.content;
                 var last_status = $('.cls-message-items').eq(-1);
@@ -16,19 +15,39 @@ $(document).ready(function () {
 });
 
 function appendStatusesList(statuses, status_template) {
-    $.each(statuses, function (index) {
+    $('<hr/>').appendTo($('#id-message-list'));
+    $.each(statuses, function (index, item) {
         var msg_item = status_template.clone();
-        if ($(this).type == 'wei') {
+        if (item.type == 'wei') {
             msg_item.attr('class', 'cls-message-items from-wei panel panel-danger')
         }
         else {
-            msg_item.attr('class', 'cls-message-items from-wei panel panel-danger')
+            msg_item.attr('class', 'cls-message-items from-twi panel panel-danger')
         }
-        alert(msg_item.html());
-        // msg_item.find('.cls-user-thumbnail').find('img').attr('src', $(this).profile);
-        // msg_item.find('.cls-user-name').text($(this).writer);
-        // msg_item.find('.cls-message-time').text($(this).time);
-        // msg_item.find('.cls-message-text').text($(this).text);
-        // msg_item.appendTo($('#id-message-list'))
+        msg_item.find('.cls-user-thumbnail').find('img').attr('src', item.profile);
+        msg_item.find('.cls-user-name').text(item.writer);
+        msg_item.find('.cls-message-time').text(moment(item.time).fromNow());
+        msg_item.find('.cls-message-text').text(item.text);
+
+        var imgs = msg_item.find('.cls-message-img');
+        if (imgs) {
+            imgs.remove()
+        }
+        var new_imgs = $('<div/>', {
+            class: 'cls-message-img'
+        });
+        $.each(item.imgs, function (index, img) {
+            $('<div/>', {
+                class: 'cls-img-wrapper'
+            }).append(
+                $('<img/>', {
+                    src: img.origi,
+                    class: "cls-img-origi"
+                })
+            ).appendTo(new_imgs)
+        });
+        new_imgs.appendTo(msg_item.find('.cls-message-content'));
+
+        msg_item.appendTo($('#id-message-list'))
     })
 }
